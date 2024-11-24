c4f8576d02c852f079ba2fba619e5bd56349425f
adkshishir <adhikarishishir50@gmail.com>
2024-11-24 23:00:22 +0545

    Revert "refactor: start port for production"
    This reverts commit 070f22d8b776d9d83bdab1de0dc68946e3054202.

--- src/components/post-editor.tsx
+++ src/components/post-editor.tsx
@@ -13,15 +13,13 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-// import CustomCKEditor from './ck-editor';
-const CustomCKEditor = dynamic(() => import('./ck-editor'), { ssr: false });
+import CustomCKEditor from './ck-editor';
 import SeoFields from './seo-fields';
 import request from '@/repo/request';
 import api from '@/repo/api';
 import toast from 'react-hot-toast';
 import { TSeo } from '@/types';
 import { useRouter } from 'next/navigation';
-import dynamic from 'next/dynamic';
 
 type PostData = {
   title: string | null;
@@ -76,14 +74,6 @@ export function PostEditorComponent({
   });
   const [writingSeo, setWritingSeo] = useState(false);
   const router = useRouter();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, [])
-  if (!mounted) {
-    return null;
-  }
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
